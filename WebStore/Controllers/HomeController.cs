02a6b83aac1708d4c65b2bd45766a27cf59063ba
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-06-30 08:16:56 +0300

    Добавлены заготовки основных страниц дизайнерского макета

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using WebStore.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-
         public IActionResult Index() => View();
 
+        public IActionResult Blogs() => View();
+
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult Checkout() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult ProductsDetails() => View();
+
+        public IActionResult Shop() => View();
     }
 }
