b016efd659527d69feff63e6a507f05c2a973882
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-02 21:45:31 +0300

    Заготовака методов для редактирвоания сотрудника

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,20 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using WebStore.Data;
-using WebStore.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
     //[Route("Users")]
     public class EmployeesController : Controller
     {
-         //[Route("User-{id}")]
+        private readonly IEmployeesData employeesData;
+        public EmployeesController(IEmployeesData employeesData)
+        {
+            this.employeesData = employeesData;
+        }
+
+        //[Route("User-{id}")]
         public IActionResult Details(int id)
         {
-            var employee = TestData.Employees.FirstOrDefault(e => e.Id == id);
+            var employee = employeesData.GetById(id);
             if (employee == null)
             {
                 return NotFound();
@@ -25,7 +27,41 @@ namespace WebStore.Controllers
         //[Route("All")]
         public IActionResult Index()
         {
-            return View(TestData.Employees);
+            return View(employeesData.Get());
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null)
+            {
+                return View(new EmployeesViewModel());
+            }
+
+            if (id < 0)
+            {
+                return BadRequest();
+            }
+
+            var employee = employeesData.GetById((int)id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+
+            return View(new EmployeesViewModel
+            {
+                id = employee.Id,
+                FirstName = employee.Name,
+                LastName = employee.Surname,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeesViewModel model)
+        {
+            return RedirectToAction(nameof(Index));
         }
     }
 }
