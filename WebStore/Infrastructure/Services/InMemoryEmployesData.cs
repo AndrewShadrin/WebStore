acc468fb750975f7df84e4893d5eefa1ae752307
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-02 20:40:51 +0300

    Заготовка сервиса управления сотрудниками

---
+++ WebStore/Infrastructure/Services/InMemoryEmployesData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Services
+{
+    public class InMemoryEmployesData : IEmployeesData
+    {
+        public int Add(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
