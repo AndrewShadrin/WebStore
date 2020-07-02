9d4bf7b61a2644739d252769f1972b1b9fba0318
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-02 20:48:34 +0300

    Переносим тестовые данные в отдельное место

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
 using WebStore.Models;
 
 namespace WebStore.Controllers
@@ -10,38 +11,10 @@ namespace WebStore.Controllers
     //[Route("Users")]
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> employees = new List<Employee>
-        {
-            new Employee
-            {
-                Id = 1,
-                Name = "Иван",
-                Surname = "Иванов",
-                Patronymic = "Иванович",
-                Age = 39
-            },
-            new Employee
-            {
-                Id = 2,
-                Name = "Пётр",
-                Surname = "Петров",
-                Patronymic = "Петрович",
-                Age = 28
-            },
-            new Employee
-            {
-                Id = 3,
-                Name = "Сидор",
-                Surname = "Сидоров",
-                Patronymic = "Сидорович",
-                Age = 18
-            },
-        };
-
-        //[Route("User-{id}")]
+         //[Route("User-{id}")]
         public IActionResult Details(int id)
         {
-            var employee = employees.FirstOrDefault(e => e.Id == id);
+            var employee = TestData.Employees.FirstOrDefault(e => e.Id == id);
             if (employee == null)
             {
                 return NotFound();
@@ -52,7 +25,7 @@ namespace WebStore.Controllers
         //[Route("All")]
         public IActionResult Index()
         {
-            return View(employees);
+            return View(TestData.Employees);
         }
     }
 }
