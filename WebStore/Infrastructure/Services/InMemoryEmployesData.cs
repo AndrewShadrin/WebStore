4339978276ca3dbaa83dc6fc223f035f8168cc72
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-02 21:09:35 +0300

    Реализация сервиса управления сотрудниками

--- WebStore/Infrastructure/Services/InMemoryEmployesData.cs
+++ WebStore/Infrastructure/Services/InMemoryEmployesData.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using WebStore.Data;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
 
@@ -9,34 +9,55 @@ namespace WebStore.Infrastructure.Services
 {
     public class InMemoryEmployesData : IEmployeesData
     {
+        private readonly List<Employee> employees = TestData.Employees;
+
         public int Add(Employee employee)
         {
-            throw new NotImplementedException();
+            if (employee is null)
+            {
+                throw new ArgumentNullException(nameof(employee));
+            }
+
+            if (employees.Contains(employee))
+            {
+                return employee.Id;
+            }
+
+            employee.Id = employees.Count == 0 ? 1 : employees.Max(e => e.Id) + 1;
+            employees.Add(employee);
+            return employee.Id;
         }
 
-        public bool Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public bool Delete(int id) => employees.RemoveAll(e => e.Id == id) > 0;
 
         public void Edit(Employee employee)
         {
-            throw new NotImplementedException();
+            if (employee is null)
+            {
+                throw new ArgumentNullException(nameof(employee));
+            }
+
+            if (employees.Contains(employee))
+            {
+                return;
+            }
+
+            var db_employee = GetById(employee.Id);
+            if (db_employee is null)
+            {
+                return;
+            }
+
+            db_employee.Name = employee.Name;
+            db_employee.Surname = employee.Surname;
+            db_employee.Patronymic = employee.Patronymic;
+            db_employee.Age = employee.Age;
         }
 
-        public IEnumerable<Employee> Get()
-        {
-            throw new NotImplementedException();
-        }
+        public IEnumerable<Employee> Get() => employees;
 
-        public Employee GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public Employee GetById(int id) => employees.FirstOrDefault(x => x.Id == id);
 
-        public void SaveChanges()
-        {
-            throw new NotImplementedException();
-        }
+        public void SaveChanges() { }
     }
 }
