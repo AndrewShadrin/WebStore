61eb47eac44112285187a0d7d618e29da92dcb13
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-03 07:18:53 +0300

    Первый метод для удаления сотрудника

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
 using WebStore.ViewModels;
 
 namespace WebStore.Controllers
@@ -23,12 +25,14 @@ namespace WebStore.Controllers
             }
             return View(employee);
         }
-        
+
         //[Route("All")]
         public IActionResult Index()
         {
             return View(employeesData.Get());
         }
+
+        #region Edit
 
         public IActionResult Edit(int? id)
         {
@@ -61,7 +65,62 @@ namespace WebStore.Controllers
         [HttpPost]
         public IActionResult Edit(EmployeesViewModel model)
         {
+            if (model is null)
+            {
+                throw new ArgumentNullException(nameof(model));
+            }
+
+            var employee = new Employee
+            {
+                Id = model.id,
+                Name = model.FirstName,
+                Surname = model.LastName,
+                Patronymic = model.Patronymic,
+                Age = model.Age
+            };
+
+            if (model.id == 0)
+            {
+                employeesData.Add(employee);
+            }
+            else
+            {
+                employeesData.Edit(employee);
+            }
+            employeesData.SaveChanges();
+
             return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var employee = employeesData.GetById(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            
+            return View(new EmployeesViewModel
+            {
+                id = employee.Id,
+                FirstName = employee.Name,
+                LastName = employee.Surname,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age
+            });
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+
         }
     }
 }
