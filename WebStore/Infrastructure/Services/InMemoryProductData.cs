ec2ddfd8f25d0561cba6ab3ad172cd0e9f5d5890
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-06 23:20:26 +0300

    Реализация выгрузки товаров из сервиса каталога товаров с возможностью фильтрации

--- WebStore/Infrastructure/Services/InMemoryProductData.cs
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Infrastructure.Interfaces;
 
@@ -10,6 +12,23 @@ namespace WebStore.Infrastructure.Services
         public IEnumerable<Brand> GetBrands()
         {
             return TestData.Brands;
+        }
+
+        public IEnumerable<Product> GetProducts(ProductFilter filter = null)
+        {
+            var query = TestData.Products;
+
+            if (filter?.SectionId != null)
+            {
+                query = query.Where(product => product.SectionId == filter.SectionId);
+            }
+
+            if (filter?.BrandId != null)
+            {
+                query = query.Where(product => product.BrandId == filter.BrandId);
+            }
+
+            return query;
         }
 
         public IEnumerable<Section> GetSections()
