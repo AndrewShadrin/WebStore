70ee22f4099dd38aa5177f8ed54dc6aa73a6c353
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-09 20:01:49 +0300

    Добавление аннотаций к сущностям для помещения их в БД посредством EF Core

--- WebStore.Domain1/Entities/Brand.cs
+++ WebStore.Domain1/Entities/Brand.cs
@@ -1,10 +1,16 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    [Table("ProductBrands")]
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
