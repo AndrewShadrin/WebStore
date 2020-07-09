848995cb325c0dc6c362ad72ede2aca6dd5132f1
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-09 20:15:37 +0300

    Заготовка класса-контекста БД

---
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreDB : DbContext
+    {
+        public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options)
+        {
+
+        }
+    }
+}
