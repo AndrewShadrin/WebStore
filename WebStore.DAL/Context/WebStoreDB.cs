944da981b6863400aa447c8dc1b5e71fb01d60bb
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-09 20:22:56 +0300

    Контекст БД готов

--- WebStore.DAL/Context/WebStoreDB.cs
+++ WebStore.DAL/Context/WebStoreDB.cs
@@ -2,11 +2,18 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using WebStore.Domain.Entities;
 
 namespace WebStore.DAL.Context
 {
     public class WebStoreDB : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
         public WebStoreDB(DbContextOptions<WebStoreDB> options) : base(options)
         {
 
