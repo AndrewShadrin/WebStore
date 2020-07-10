5dbf876870d893f1367e63ac4e67c8527622e2e6
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-10 08:23:20 +0300

    Добавлена заготовка пользователя системы

---
+++ WebStore.Domain1/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities
+{
+    public class User : NamedEntity
+    {
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+        public DateTime EmploymentDate { get; set; }
+
+        public bool IsEmployee { get => EmploymentDate.Ticks != 0; }
+    }
+}
