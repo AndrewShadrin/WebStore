39ae9a64db426bbb1aaf5b7d0ffd2d9831391c46
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-12 14:18:54 +0300

    Заготовка контроллера Account

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        public IActionResult Register() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult Logout() => RedirectToAction("Index", "Home");
+
+        public IActionResult AccessDenied() => View();
+    }
+}
