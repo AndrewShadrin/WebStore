b8e34ae3d511e230188ee6dcce6b7e5478529c0e
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-12 10:51:05 +0300

    Перенос сущности Сотрудник в доменную область

--- WebStore.Domain1/Entities/Employee.cs
+++ WebStore.Domain1/Entities/Employee.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base;
 
-namespace WebStore.Models
+namespace WebStore.Domain.Entities
 {
-    public class Employee
+    public class Employee 
     {
         public int Id { get; set; }
         public string Name { get; set; }
