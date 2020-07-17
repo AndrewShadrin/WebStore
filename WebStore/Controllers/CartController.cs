d3367f81cd1ce90d5345d8400bc411dad648d400
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-17 11:15:06 +0300

    Заготовка контроллера корзины

---
+++ WebStore/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService cartService;
+
+        public CartController(ICartService cartService)
+        {
+            this.cartService = cartService;
+        }
+
+        public IActionResult Details() => View(cartService.TransformFromCart());
+    }
+}
