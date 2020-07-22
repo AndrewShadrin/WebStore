24ae778dd08c4720beafdd5f5dce15792cabdff7
Andrew Shadrin <andrew_shadrin@mail.ru>
2020-07-22 20:18:14 +0300

    Заготовка профиля пользователя

---
+++ WebStore/Controllers/UserProfileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
