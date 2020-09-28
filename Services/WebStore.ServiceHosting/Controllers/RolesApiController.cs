c56ab75181ba9e682f476b19651501a2fe511470
Шадрин Андрей <andrew_shadrin@mail.ru>
2020-09-28 07:09:35 +0300

    Заготовки контроллеров WebApi

---
+++ Services/WebStore.ServiceHosting/Controllers/RolesApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebApi.Identity.Roles)]
+    [ApiController]
+    public class RolesApiController : ControllerBase
+    {
+        private readonly RoleStore<Role> roleStore;
+
+        public RolesApiController(WebStoreDB dB)
+        {
+            roleStore = new RoleStore<Role>(dB);
+        }
+    }
+}
---
+++ Services/WebStore.ServiceHosting/Controllers/UsersApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebApi.Identity.Users)]
+    [ApiController]
+    public class UsersApiController : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreDB> userStore;
+        public UsersApiController(WebStoreDB dB)
+        {
+            userStore = new UserStore<User, Role, WebStoreDB>(dB);
+        }
+    }
+}
